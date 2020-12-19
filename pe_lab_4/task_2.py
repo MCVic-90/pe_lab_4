11cfee0e3033cf683b72224e2341fc4421be0014
Victor_Kochukov <victorkochukow90@gmail.com>
2020-12-19 17:59:36 +0300

    read file and create table for getted information

---
+++ pe_lab_4/task_2.py
@@ -0,0 +1,46 @@
+import texttable as tt
+
+path = 'C:\\Users\\MC_VIC\\Dropbox\\1 семестр\\ПИ\\лр3\\students.csv'
+
+
+class Students:
+
+    def __init__(self):
+        self.data = []
+        self.title_data = []
+
+    def read_file(self, path):
+        file = open(path, 'r', encoding='utf-8')
+        self.data = file.read()
+        file.close()
+
+        self.convert_str_to_list(self.data)
+
+    def convert_str_to_list(self, d):
+        list_of_students = d.split('\n')
+        for i in range(len(list_of_students)):
+            list_of_students[i] = list_of_students[i].split(';')
+        self.title_data = list_of_students.pop(0)
+        self.data = list_of_students
+
+        self.output()
+
+    def output(self, d=None, t=None):
+        tab = tt.Texttable()
+
+        if d == None:
+            d = self.data
+        if t == None:
+            t = self.title_data
+
+        tab.header(t)
+
+        for row in d:
+            tab.add_row(row)
+
+        table = tab.draw()
+        print(table)
+
+
+s = Students()
+s.read_file(path)
