1d8f505e4a0dcc39774c8d76089f3fbe6de951b5
Victor_Kochukov <victorkochukow90@gmail.com>
2020-12-20 03:32:52 +0300

    solve 2.3 and 2.4, but i can't delete the last line when created new file

--- pe_lab_4/task_2.py
+++ pe_lab_4/task_2.py
@@ -1,8 +1,9 @@
 from operator import itemgetter
-
+import csv
 import texttable as tt
 
 path = 'C:\\Users\\MC_VIC\\Dropbox\\1 семестр\\ПИ\\лр3\\students.csv'
+path2 = 'C:\\Users\\MC_VIC\\Dropbox\\1 семестр\\ПИ\\лр3\\students_changed.csv'
 
 
 class Students:
@@ -24,7 +25,6 @@ class Students:
             list_of_students[i] = list_of_students[i].split(';')
         self.title_data = list_of_students.pop(0)
         self.data = list_of_students
-
         self.output()
 
     def output(self, d=None, t=None):
@@ -38,17 +38,45 @@ class Students:
 
         tab.header(t)
 
-        ##for row in d:                                       ##table with all info
-            ##tab.add_row(row)
+        for row in d:                                       ##table with all info
+            tab.add_row(row)
 
-        n = int(input("Введите возрастной ограничитель: ")) ##enter "n"
-        for row in d:
-            if int(row[2]) >= n:                            ##table with older "n" age
-                tab.add_row(row)
+        ##n = int(input("Введите возрастной ограничитель: ")) ##enter "n"
+        ##for row in d:
+            ##if int(row[2]) >= n:                            ##table with older "n" age
+                ##tab.add_row(row)
 
         table = tab.draw()
         print(table)
 
+    def add_new_info(self):                                   ##add new student and write new file
+
+        new_number = self.data[-1][0]
+        new_fio = input("Введите ФИО нового студента: ")
+        new_age = input("Введите возраст нового студента: ")
+        new_group = input("Введите группу нового студента: ")
+
+        with open(path, encoding='utf-8') as r_file:
+            old_data = []
+            reader = csv.reader(r_file, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+            for i in reader:
+                old_data.append(i)
+
+        new_data = old_data + [[str(int(new_number)+1),
+                               str(new_fio),
+                               str(new_age),
+                               str(new_group)]]
+
+        new_csv = str(path[:-4] + '_changed.csv')
+
+        with open(new_csv, 'w', encoding='utf-8', newline='') as n_file:
+            writer = csv.writer(n_file, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+            writer.writerows(new_data)
+
+        print("Новые данные сохранены в новый файл " + new_csv + "!")
+
 
 s = Students()
 s.read_file(path)
+s.add_new_info()
+s.read_file(path2)
